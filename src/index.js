7f236a21a4db0c1b0364b91e930806b735fe0aea
jaiswaladi2468 <567adddi.jais@gmail.com>
2023-09-22 19:58:58 +0530

    Added source code

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+import 'normalize.css';
+import App from './app';
+import { GlobalStyles } from './global-styles';
+import { firebase } from './lib/firebase.prod';
+import { FirebaseContext } from './context/firebase';
+
+render(
+    <>
+    <FirebaseContext.Provider value={{ firebase }}>
+        <GlobalStyles />
+        <App />
+    </FirebaseContext.Provider>
+    </>,
+    document.getElementById('root')
+);
